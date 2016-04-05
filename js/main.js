6e897b8fc54df81cfd2327b66af750ae28acf640
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-04-05 13:06:45 -0400

    we

--- js/main.js
+++ js/main.js
@@ -11,7 +11,3 @@ for (i = 0; i < 75; i++) {
     left: Math.random() * bannerWidth,
     opacity: Math.random(),
     transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
-
-  });
-  $body.append($star);
-}
