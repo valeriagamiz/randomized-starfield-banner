4d5808bbd86303eebc0593918254c49218d5e809
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-04-05 13:17:35 -0400

    again

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
-var $banner = $('.banner');
-var $addStars;
-var $width = $banner.outerWidth();
-var $height = $banner.outerHeight();
-
-
+var $body = $('.banner');
+var $star;
+var bannerWidth = $body.outerWidth();
+var bannerHeight = $body.outerHeight();
 
 for (i = 0; i < 75; i++) {
-  $rotateNum = Math.random() * 1000;
-  $scaleNum = Math.random() * 10000;
-  $animateNum = Math.random() * 50;
-  $addStars = $('<div>');
-  $addStars.addClass('star');
-  $addStars.css({
-    left: Math.random() * 1000,
-    top: Math.random() * 1000,
-    opacity: Math.random() * 10,
-    width: Math.random() * 100,
-    transform: 'rotate(' + $rotateNum + 'deg)',
-    transform: 'scale(' + $scaleNum + '%)',
-    animation: 'growstars ' + $animateNum + 's alternate infinite'
-  });
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+    top: Math.random() * bannerHeight,
+    left: Math.random() * bannerWidth,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
 
- $banner.append($addStars);
-};
+  });
+  $body.append($star);
+}
