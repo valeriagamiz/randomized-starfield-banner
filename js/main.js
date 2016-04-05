8641bc33907603048aa52a71fcae05332309f2e1
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-04-05 13:05:43 -0400

    css

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var $body = $('.banner');
+var $star;
+var bannerWidth = $body.outerWidth();
+var bannerHeight = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+    top: Math.random() * bannerHeight,
+    left: Math.random() * bannerWidth,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+
+  });
+  $body.append($star);
+}
